b55d36dac18216af77932175088cde4219e19639
ngotruonghai <52746475+ngotruonghai@users.noreply.github.com>
2019-11-13 13:30:49 +0700

    no message

--- Nganhtinhoc/Nganhtinhoc/Areas/admin/Models/trangController.cs
+++ Nganhtinhoc/Nganhtinhoc/Areas/admin/Models/trangController.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using Nganhtinhoc.Areas.admin.Controllers;
 namespace Nganhtinhoc.Areas.admin.Models
 {
-    public class trangController : Controller
+    public class trangController : baseeController
     {
         // GET: admin/trang
         public ActionResult Index()
