99f67b1b1801918033cfb6190832d1a2a95ba9f5
ngotruonghai <52746475+ngotruonghai@users.noreply.github.com>
2020-01-02 21:54:59 +0700

    no message

---
+++ Nganhtinhoc/Nganhtinhoc/Areas/admin/Controllers/NganhsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Nganhtinhoc.Models;
+
+namespace Nganhtinhoc.Areas.admin.Controllers
+{
+    public class NganhsController : Controller
+    {
+        private NTHEntities db = new NTHEntities();
+
+        // GET: admin/Nganhs
+        public ActionResult Index()
+        {
+            return View(db.Nganh.ToList());
+        }
+
+        // GET: admin/Nganhs/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Nganh nganh = db.Nganh.Find(id);
+            if (nganh == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nganh);
+        }
+
+        // GET: admin/Nganhs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: admin/Nganhs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "manganh,tennganh,khoahoc")] Nganh nganh)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Nganh.Add(nganh);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(nganh);
+        }
+
+        // GET: admin/Nganhs/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Nganh nganh = db.Nganh.Find(id);
+            if (nganh == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nganh);
+        }
+
+        // POST: admin/Nganhs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "manganh,tennganh,khoahoc")] Nganh nganh)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(nganh).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(nganh);
+        }
+
+        // GET: admin/Nganhs/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Nganh nganh = db.Nganh.Find(id);
+            if (nganh == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nganh);
+        }
+
+        // POST: admin/Nganhs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Nganh nganh = db.Nganh.Find(id);
+            db.Nganh.Remove(nganh);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
